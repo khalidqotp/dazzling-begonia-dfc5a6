4f25525b58377c286275fac9556a60792705905a
khalidqotp <khalidqotp@gmail.com>
2026-06-16 22:47:35 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,18 +3,16 @@ import { tanstackStart } from '@tanstack/react-start/plugin/vite'
 import viteReact from '@vitejs/plugin-react'
 import viteTsConfigPaths from 'vite-tsconfig-paths'
 import tailwindcss from '@tailwindcss/vite'
-import vercel from '@vercel/tanstack-start' // 1. استيراد بلجن فيرسيل بدلاً من نيتلفاي
 
-const config = defineConfig({
+export default defineConfig({
   plugins: [
     viteTsConfigPaths({
       projects: ['./tsconfig.json'],
     }),
     tailwindcss(),
-    vercel(), // 2. تغيير البلجن هنا لفيرسيل
-    tanstackStart(),
+    tanstackStart({
+      deploymentTarget: 'vercel-serverless'
+    }),
     viteReact(),
   ],
 })
-
-export default config
