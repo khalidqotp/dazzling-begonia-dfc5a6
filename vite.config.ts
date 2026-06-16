9271a803672f6736d347cbcb67cdba0730a4cd77
khalidqotp <khalidqotp@gmail.com>
2026-06-16 22:51:18 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -10,9 +10,7 @@ export default defineConfig({
       projects: ['./tsconfig.json'],
     }),
     tailwindcss(),
-    tanstackStart({
-      deploymentTarget: 'vercel-serverless'
-    }),
+    tanstackStart(),
     viteReact(),
   ],
 })
